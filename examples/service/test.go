e7ee740608fe6f782498acdef001a3d27089e123
ftsai231 <f84024048@gamil.com>
2019-11-21 14:48:37 -0800

    test

--- examples/service/test.go
+++ examples/service/test.go
@@ -9,6 +9,9 @@ import (
 
 func NewTestService() *gatt.Service {
 	n := 0
+
+	var str string = "testing string"
+
 	s := gatt.NewService(gatt.MustParseUUID("09fc95c0-c111-11e3-9904-0002a5d5c51c"))
 
 
@@ -17,14 +20,19 @@ func NewTestService() *gatt.Service {
 
 		func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
 			fmt.Fprintf(rsp, "count: %d", n)
+			fmt.Fprintf(rsp, str)
 			fmt.Println( "count: %d", n)
+
 			n++
 		})
 
-	gatt.NewCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51d"), s, 0x02, 0, 0)
+	c := gatt.NewCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51d"), s, 0x02, 0, 0)
 
 	s.AddCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51d"))
-	
+
+	d := gatt.NewDescriptor(gatt.MustParseUUID("2901"), 0,  c)
+
+	//d.SetValue()
 
 	return s
 }
