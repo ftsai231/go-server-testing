dd4122f07d20940f01d2ef17c87014e51bfe747b
ftsai231 <f84024048@gamil.com>
2019-11-22 14:58:08 -0800

    test

--- examples/service/test.go
+++ examples/service/test.go
@@ -1,6 +1,5 @@
 package service
 
-import "C"
 import (
 	"fmt"
 	"github.com/paypal/gatt"
@@ -24,6 +23,7 @@ func NewTestService() *gatt.Service {
 	s.AddCharacteristic(gatt.MustParseUUID("5435D20C-7086-484A-B506-9234873070EA")).HandleReadFunc(
 		func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
 			fmt.Println( "(Println) Characteristic Name: " + c.Name())
+			fmt.Println("Descriptor UUID:" + d.UUID().String())
 			fmt.Println("value byte array: ", []byte("Hello World"))
 		})
 
