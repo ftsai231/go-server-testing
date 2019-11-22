e48e581897c44ca0e0a0784f625c33b9314dff2d
ftsai231 <f84024048@gamil.com>
2019-11-21 17:24:18 -0800

    test

--- examples/service/test.go
+++ examples/service/test.go
@@ -1,6 +1,5 @@
 package service
 
-
 import (
 	"fmt"
 
@@ -9,15 +8,9 @@ import (
 
 func NewTestService() *gatt.Service {
 	n := 0
-
 	var str string = "testing string"
-
 	s := gatt.NewService(gatt.MustParseUUID("09fc95c0-c111-11e3-9904-0002a5d5c51c"))
-
-
 	s.AddCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51c")).HandleReadFunc(
-
-
 		func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
 			fmt.Fprintf(rsp, "count: %d", n)
 			fmt.Fprintf(rsp, str)
@@ -27,12 +20,13 @@ func NewTestService() *gatt.Service {
 		})
 
 	c := gatt.NewCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51d"), s, 0x01 | 0x02 | 0x08, 0, 0)
-
-	//gatt.Property()
-
 	d := gatt.NewDescriptor(gatt.MustParseUUID("2901"), 0,  c)
-
 	d.SetValue([]byte("Hello World"))
+
+
+
+	c.AddDescriptor(gatt.UUID16(0x2904)).SetValue([]byte{4, 1, 39, 173, 1, 0, 0})
+
 
 	s.AddCharacteristic(gatt.MustParseUUID("11fac9e0-c111-11e3-9246-0002a5d5c51d")).HandleReadFunc(
 		func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
@@ -40,8 +34,6 @@ func NewTestService() *gatt.Service {
 			fmt.Println(str)
 
 		})
-
-
 
 	return s
 }
