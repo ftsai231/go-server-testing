cf14af533f6d67cc1de5344784d67f2a5b466d4d
ftsai231 <f84024048@gamil.com>
2019-11-22 11:32:12 -0800

    test

--- examples/service/test.go
+++ examples/service/test.go
@@ -18,9 +18,8 @@ func NewTestService() *gatt.Service {
 	c := gatt.NewCharacteristic(gatt.MustParseUUID("5435D20C-7086-484A-B506-9234873070EA"), s, 0x01 | 0x02 | 0x08, 0, 0)
 	d := gatt.NewDescriptor(gatt.UUID16(0x2901), 0,  c)
 	d.SetValue([]byte("Hello World"))
-
-	//c.AddDescriptor(gatt.UUID16(0x2904)).SetValue([]byte{4, 1, 39, 173, 1, 0, 0})
-
+	c.AddDescriptor(gatt.UUID16(0x2901))
+	
 	s.AddCharacteristic(gatt.MustParseUUID("5435D20C-7086-484A-B506-9234873070EA"))
 
 	return s
